3462efde6edd5263643a425def41ec04e00b1218
Shuhratjan <nshuhr@gmail.com>
2019-01-17 13:11:28 +0600

    Main Tasks completed

--- ServicePacket/wwwroot/js/site.js
+++ ServicePacket/wwwroot/js/site.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
     var totalPrice = 0;
     $(".serviceBox").click(function () {
-        if ($(this).attr("data-type")==="item") {
+        if ($(this).attr("data-type") === "item") {
             if ($(this).hasClass("checked")) {
                 $(this).removeClass("checked");
                 $("div#receiptList div.item[data-id='" + $(this).attr("data-id") + "']").remove();
@@ -15,7 +15,7 @@ $(document).ready(function () {
         } else {
             if ($(this).hasClass("checked")) {
                 $(this).removeClass("checked");
-                if ($(this).attr("data-period-id") === "2"){
+                if ($(this).attr("data-period-id") === "2") {
                     $("div#period div.item").attr("data-period-id", "1");
                     $("div#period div.item span").html("1 месяц");
                 }
@@ -31,10 +31,32 @@ $(document).ready(function () {
                 $("div#period div.item").attr("data-period-id", $(this).attr("data-period-id"));
                 $(this).addClass("checked");
             }
+            
         }
 
 
+        if ($("#receiptList > div").length > 1) {
+            totalPrice = 0;
+            $("#receiptList div").each(function (index) {
+                var attr = $(this).attr('data-id');
+                if (typeof attr !== typeof undefined && attr !== false) {
+                    totalPrice += parseInt($(this).find('span').text(), 10);
+                }
+            });
+            if ($("#period div.item").attr("data-period-id") === "2") {
+                totalPrice = totalPrice - totalPrice * 12 / 100;
+            }
+            $("#totalPrice span#price").text(totalPrice + "тг");
+        }
         
-        
+    });
+
+    $("#myButton").click(function () {
+        if ($("input[type='checkbox']")[0].checked && $("#receiptList > div").length > 1) {
+            swal("Ваша заявка принята", "С Вами свяжутся наши менеджеры.", "success");
+        } else {
+            swal("Ошибка", "Не выбран услуги или нет галочки в поле соглашение", "error");
+        }
+
     });
 });
