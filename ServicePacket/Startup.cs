01d2e7b8f3fcc3e60641d2bafe38184fd84e175e
shuhratjan <43290849+shuhratjan@users.noreply.github.com>
2019-01-18 12:22:50 +0600

    Update Startup.cs

--- ServicePacket/Startup.cs
+++ ServicePacket/Startup.cs
@@ -63,7 +63,7 @@ namespace ServicePacket
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Service}/{action=Index}/{id?}");
             });
         }
     }
